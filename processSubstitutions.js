bb3d5a523af64da0b3817c82bd6ebcfa18bd0bb7
Andriy Kashcha <anvaka@gmail.com>
2018-12-09 12:49:13 -0800

    simplified replacement logic

--- processSubstitutions.js
+++ processSubstitutions.js
@@ -3,17 +3,18 @@ var path = require('path');
 
 var substitutions = require('./1/substitutes.json');
 var caseLookup = createCaseLookup();
-var correctedSubstitutions = substitutions.map(sub => {
-  return sub.filter(x => caseLookup.has(x.toLowerCase()))
+
+var subreddits = new Map();
+substitutions.forEach(sub => {
+  const subWith = sub.filter(x => caseLookup.has(x.toLowerCase()))
     .map(related => caseLookup.get(related.toLowerCase()))
+  
+  subreddits.set(subWith[0], subWith);
 })
 var fileNames = getFileNames();
 
-var substitutionsByLetter = indexSubstitutionsByLetter(correctedSubstitutions);
-console.log(substitutionsByLetter);
-
-substitutionsByLetter.forEach((subreddits, letter) => {
-  let file = getFileContentForLetter(letter);
+fileNames.forEach(fileName => {
+  let file = getFileContent(fileName);
   let out = [];
   file.content.forEach(row => {
     const subName = row[0];
@@ -24,13 +25,14 @@ substitutionsByLetter.forEach((subreddits, letter) => {
       out.push(row);
     }
   });
+
   fs.writeFileSync(path.join(__dirname, 'out', file.name), JSON.stringify(out), 'utf8');
 });
 
-function getFileContentForLetter(letter) {
-  const fullName = path.join(__dirname, '1', fileNames.get(letter));
+function getFileContent(fileName) {
+  const fullName = path.join(__dirname, '1', fileName);
   const result = {
-    name: fileNames.get(letter),
+    name: fileName,
     content: require(fullName)
   };
   return result;
@@ -47,31 +49,8 @@ function createCaseLookup() {
   return lowerCaseToRealCase;
 }
 
-function indexSubstitutionsByLetter(subs) {
-  const subByLetter = new Map(); // maps first sub reddit to map of reddits to replace
-  subs.forEach(row => {
-    const subreddit = row[0];
-    let firstLetter = subreddit[0].toLowerCase(); 
-    let subs =subByLetter.get(firstLetter);
-    if (!subs) {
-      subs = new Map(); // subreddit name => list of subreddits.
-      subByLetter.set(firstLetter, subs);
-    }
-    subs.set(subreddit, row);
-  });
-  return subByLetter;
-}
-
 function getFileNames() {
-  let fileNamesIndex = new Map();
-[
-  '0_z0123456789jqx.json', '10_m.json', '11_l.json', '12_i.json', '13_h.json', '14_g.json', '15_f.json', '16_e.json', '17_d.json', '18_c.json', '19_b.json', '1_yk.json', '20_a.json', '2_w.json', '3_vo.json', '4_u.json', '5_t.json', '6_s.json', '7_r.json', '8_p.json', '9_n.json'
-].forEach(name => {
-    const fileName = name.replace(/^\d\d?_/, '').replace(/\.json$/, '');
-    for (var i = 0; i < fileName.length; ++i) {
-      let letter = fileName[i];
-      fileNamesIndex.set(letter, name);
-    }
-  });
-  return fileNamesIndex;
+  return [
+    '0_z0123456789jqx.json', '10_m.json', '11_l.json', '12_i.json', '13_h.json', '14_g.json', '15_f.json', '16_e.json', '17_d.json', '18_c.json', '19_b.json', '1_yk.json', '20_a.json', '2_w.json', '3_vo.json', '4_u.json', '5_t.json', '6_s.json', '7_r.json', '8_p.json', '9_n.json'
+  ];
 }
